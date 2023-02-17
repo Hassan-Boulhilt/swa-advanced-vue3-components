9158f270e429e457291302fdac388ecda9b6c322
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-17 07:46:45 +0100

    Props make communication between components possible

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <p>Red</p>
-  <Greeting></Greeting>
+  <h3>Hey!</h3>
+  <greeting></greeting>
+  <user :age="age"></user>
 </template>
 
 <script>
 import Greeting from "@/components/Greeting.vue";
+import User from "@/components/User.vue";
 export default {
   name: "App",
   components: {
     Greeting,
+    User,
   },
+  data(){
+    return {
+        age: 20
+    }
+  }
 };
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <p>The user is {{ age }} years old</p>
+
+    
+</template>
+
+<script>
+    export default {
+        name:'user',
+        props: ["age"]
+        
+        
+    }
+</script>
+
