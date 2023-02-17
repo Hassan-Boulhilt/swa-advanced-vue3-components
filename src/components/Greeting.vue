3f7f092b02f8d7a3f40863af7f9ced8cbadfeab5
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-17 07:21:53 +0100

    Add Sass to component

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -14,9 +14,13 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 p {
     color:red;
+
+    &:hover {
+        color: darken($color: #CC4444, $amount: 15%)
+    }
 }
 
 </style>
